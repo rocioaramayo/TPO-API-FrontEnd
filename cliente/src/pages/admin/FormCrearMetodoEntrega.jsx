6f84eb2e4dca1c352027437608fcb59996ec77c8
GuidoLevy <guidoalevy@gmail.com>
2025-06-03 11:30:43 -0300

    crear metodo empezado

---
+++ cliente/src/pages/admin/FormCrearMetodoEntrega.jsx
@@ -0,0 +1,7 @@
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+const FormCrearMetodoEntrega = () => {
+}
+
+export default FormCrearMetodoEntrega;
